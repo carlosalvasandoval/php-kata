5b99f8957c288fdffdbbcb9d2a236aaecd17bcf3
carlosalvasandoval <carlos.alva.sandoval@gmail.com>
2020-10-13 23:32:52 +0000

    init config

---
+++ src/Greeting.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace main;
+
+class Greeting
+{
+    public function hello()
+    {
+        return 'hello';
+    }
+
+    public function bye()
+    {
+        return 'bye';
+    }
+}
